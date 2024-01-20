46547343fb239f027cfda0bdac2a21ccbe59789c
Prathamesh Rokade <prathameshr955@gmail.com>
2024-01-20 22:20:28 +0530

    first commit

---
+++ app1.js
@@ -0,0 +1,22 @@
+console.log("Hello World!")
+
+var name1 = "MyName";
+
+let age =  19;
+
+const CITY = "Nagpur";
+
+console.log(name1);
+
+const myObject = {
+    name: "Prem",
+    age: 19,
+    city: "Nagpur",
+}; 
+
+console.log( myObject.name);
+console.log(myObject);
+
+const label = `This is sample ${myObject.name}`;
+console.log(label);
+
